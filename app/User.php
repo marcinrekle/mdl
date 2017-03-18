def771cb6bb1bd4b95e449b0502c45a45b1e6c3c
marcinrekle <marcinrekle@gmail.com>
2017-03-18 16:37:14 +0100

    add fillable fields

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'social_id', 'avatar', 'remember_token', 'social_token', 'confirmed', 'confirm_code'
     ];
 
     /**
