7600d6e08b3e0646a5f0b5f62889db9132acd40c
marcinrekle <marcinrekle@gmail.com>
2017-03-18 10:59:58 +0100

    add osk model and controller, add user controller and add auth

---
+++ app/Http/Controllers/OskController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Osk;
+use Illuminate\Http\Request;
+
+class OskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Osk  $osk
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Osk $osk)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Osk  $osk
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Osk $osk)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Osk  $osk
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Osk $osk)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Osk  $osk
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Osk $osk)
+    {
+        //
+    }
+}
