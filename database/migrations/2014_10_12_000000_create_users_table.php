8b3492ce2d64046eb2836b4bad311831c9662817
marcinrekle <marcinrekle@gmail.com>
2017-03-18 09:36:51 +0100

    edit users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->increments('id')->unsigned();
+            $table->string('name',128);
+            $table->string('email',64);
+            $table->string('password',64);
+            $table->string('social_id',32)->nullable();
+            $table->string('avatar',128);
+            $table->string('remember_token',128)->nullable();
+            $table->string('social_token',128)->nullable();
+            $table->boolean('confirmed')->default(0);
+            $table->string('confirm_code', 32);
             $table->timestamps();
         });
     }
