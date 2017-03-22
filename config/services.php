16e84368fafe127831a5df35b16a5594bdbbf60e
marcinrekle <marcinrekle@gmail.com>
2017-03-22 09:35:26 +0100

    add socialate providers

--- config/services.php
+++ config/services.php
@@ -35,4 +35,22 @@ return [
         'secret' => env('STRIPE_SECRET'),
     ],
 
+    'github' => [
+        'client_id' => 'your-github-app-id',
+        'client_secret' => 'your-github-app-secret',
+        'redirect' => 'http://mdl.dev/auth/github/callback',
+    ],
+
+    'facebook' => [
+        'client_id' => '1510886492549477',
+        'client_secret' => '41f683bc22a73c172510e541134e3d87',
+        'redirect' => 'http://mdl.dev/auth/facebook/callback',
+    ],
+
+    'gmail' => [
+        'client_id' => '351246343118-ijvcjo42qkot0hae1fsrmp0kf0661uft.apps.googleusercontent.com',
+        'client_secret' => '2rdk-kJb55uaFTtRRSid98Nr',
+        'redirect' => 'http://mdl.dev/auth/gmail/callback',
+    ],
+
 ];
