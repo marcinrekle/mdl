cace98a7b685142fbbda2e53f8edfc80a01a0e7a
marcinrekle <marcinrekle@gmail.com>
2017-03-22 09:36:54 +0100

    add socialite routes

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
 Route::get('/users/confirm/{confirm_code}');
 
 Route::get('/home', 'HomeController@index');
