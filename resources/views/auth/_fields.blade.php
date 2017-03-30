db47ec8474c9254b6f00f55099ffe831c5bad567
marcinrekle <marcinrekle@gmail.com>
2017-03-30 10:40:53 +0200

    create register extra fields view

---
+++ resources/views/auth/_fields.blade.php
@@ -0,0 +1,8 @@
+@foreach($fields as $field)
+	<div class="form-group">
+		<label for="{{ $field->name }}" class="col-md-4 control-label">{{ $field->slug }}</label>
+		<div class="col-md-6">
+            <input id="{{ $field->name }}" type="{{ $field->type }}" class="form-control" name="{{ $field->name }}" {{ $field->require ? 'required' : '' }}>
+        </div>
+	</div>
+@endforeach
