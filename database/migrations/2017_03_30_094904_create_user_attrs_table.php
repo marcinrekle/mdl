bac4980dea1b878ee46f11c91da577728f04c572
marcinrekle <marcinrekle@gmail.com>
2017-03-30 17:18:13 +0200

    create migration users_attrs

---
+++ database/migrations/2017_03_30_094904_create_user_attrs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserAttrsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_attrs', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->text('value',512);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_attrs');
+    }
+}
