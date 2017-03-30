719c1aa49be6446996ee63a618cde1bf46bd7b78
marcinrekle <marcinrekle@gmail.com>
2017-03-30 10:40:09 +0200

    create social login button view

---
+++ resources/views/auth/_social.blade.php
@@ -0,0 +1,11 @@
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+		<div class="panel panel-default">
+    		<div class="panel-heading">Logowanie przy pomocy portalu społeczniościowego</div>
+    		<div class="panel-body">
+        		<a href="{{url('/auth/facebook')}}" class="btn btn-primary" role="button"><i class="fa fa-facebook-official fa-lg"></i> Zaloguj z Facebook</a>
+        		<a href="{{url('/auth/google')}}" class="btn btn-danger" role="button"><i class="fa fa-google fa-lg"></i> Zaloguj z Gmail</a>
+    		</div>
+    	</div>
+	</div>
+</div>
